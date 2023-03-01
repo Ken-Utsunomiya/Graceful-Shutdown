583b570459ae80f386e4b0c0acc154406fd96b0e
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2023-03-01 11:58:32 +0900

    feat: implement TrackRequest middleware

--- handler/middleware/track_process.go
+++ handler/middleware/track_process.go
@@ -1,1 +1,15 @@
 package middleware
+
+import (
+	"net/http"
+	"sync"
+)
+
+func TrackRequest(h http.Handler, wg *sync.WaitGroup) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		wg.Add(1)
+		defer wg.Done()
+		h.ServeHTTP(w, r)
+	}
+	return http.HandlerFunc(fn)
+}
